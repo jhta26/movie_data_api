a489d098eb4154c1924fc317d1ab099e5e2666ba
jhta26 <hsujasonf@gmail.com>
2018-05-15 11:11:24 -0700

    setting up controllers and services

--- env.js
+++ env.js
@@ -10,5 +10,6 @@ module.exports = {
   PGUSER: process.env.PGUSER,
   PGPASSWORD: process.env.PGPASSWORD,
   PGDATABASE: process.env.PGDATABASE,
-  JWT_KEY: process.env.JWT_KEY
+  JWT_KEY: process.env.JWT_KEY,
+  API_KEY:'4d5bf8d10c6b03d87d1ad39ff4d7e296'
 };
---
+++ library/controllers/MovieAPIController.js
@@ -0,0 +1,17 @@
+export default class MovieAPIController {
+    constructor() {
+
+    }
+
+    async getMovieInfoTMDB(searchInput) {
+        let TMDBRequest = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${env.API_KEY}&language=en-US&query=${
+        searchInput
+      }&page=1&include_adult=false`)
+        let TMDBResponse = await TMDBRequest.json()
+        let results = await TMDBResponse.results
+        return results
+
+    }
+}
+
+
