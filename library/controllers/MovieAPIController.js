eff2d9685d674cbdf95bbcc9b4cc27b98ab60af5
jhta26 <hsujasonf@gmail.com>
2018-05-16 19:08:30 -0700

    adding upcoming movies route

--- library/controllers/MovieAPIController.js
+++ library/controllers/MovieAPIController.js
@@ -16,6 +16,23 @@ class MovieAPIController {
         let responseObject = TMDBResponse
         res.set({ 'access-content-allow-origin': ['*'] }).status(200).json(responseObject);
     }
+    async getPopularMovies(req, res, next) {
+        let TMDBRequest = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&page=1`
+        );
+        let TMDBResponse = await TMDBRequest.json();
+        let results = await TMDBResponse.results;
+        return results;
+    };
+    async getUpcomingMovies(req, res, next) {
+        let TMDBRequest = await fetch(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&page=1`
+        );
+        let TMDBResponse = await TMDBRequest.json();
+        let results = await TMDBResponse.results;
+        return results;
+    };
+
 }
 
 module.exports = MovieAPIController
--- routes/movieAPI.js
+++ routes/movieAPI.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const movieAPIController = require('../library/instances/movieAPI/movieAPIController')
 
 router.get('/movieapi/:search', movieAPIController.getMovieInfoTMDB)
+router.get('/movieapi/popular',movieAPIController.getPopularMovies)
+router.get('/movieapi/upcoming',movieAPIController.getUpcomingMovies)
 router.all('/users/:user_id(\\d+)/ratings', (req, res, next) => {
   res.status(405).send('Method Not Allowed');
 });
