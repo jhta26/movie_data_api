490917caa0c10a4e657e14898b5f90eb5f80e40e
jhta26 <hsujasonf@gmail.com>
2018-05-17 18:51:45 -0700

    fixing env files

--- env.js
+++ env.js
@@ -11,5 +11,5 @@ module.exports = {
   PGPASSWORD: process.env.PGPASSWORD,
   PGDATABASE: process.env.PGDATABASE,
   JWT_KEY: process.env.JWT_KEY,
-  API_KEY:'4d5bf8d10c6b03d87d1ad39ff4d7e296'
+  API_KEY:process.env.API_KEY
 };
--- library/controllers/MovieAPIController.js
+++ library/controllers/MovieAPIController.js
@@ -11,26 +11,36 @@ class MovieAPIController {
         };
         let TMDBRequest = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&query=${
         req.params.search
-      }&page=1&include_adult=false`, myInit)
+      }&page=1&include_adult=false&region=US`, myInit)
         let TMDBResponse = await TMDBRequest.json()
         let responseObject = TMDBResponse
         res.set({ 'access-content-allow-origin': ['*'] }).status(200).json(responseObject);
     }
     async getPopularMovies(req, res, next) {
+        var myInit = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        };
         let TMDBRequest = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&page=1`
-        );
+            `https://api.themoviedb.org/3/movie/popular?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&region=US&language=en-US&page=1`, myInit);
         let TMDBResponse = await TMDBRequest.json();
         let results = await TMDBResponse.results;
-        return results;
+
+        res.set({ 'access-content-allow-origin': ['*'] }).status(200).json(results);
     };
     async getUpcomingMovies(req, res, next) {
+        var myInit = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        };
         let TMDBRequest = await fetch(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&page=1`
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=4d5bf8d10c6b03d87d1ad39ff4d7e296&language=en-US&region=US&page=1`
         );
         let TMDBResponse = await TMDBRequest.json();
         let results = await TMDBResponse.results;
-        return results;
+        res.set({ 'access-content-allow-origin': ['*'] }).status(200).json(results);
     };
 
 }
